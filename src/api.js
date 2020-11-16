7da5c5ba82ef4d3221559d9eeaabd7e3f4454586
SofianePean <pean.sofiane@gmail.com>
2020-11-16 14:25:20 +0100

    change token

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     // 'Client-ID': process.env.CONFIG_CLIENT,
     'Client-ID': 'plfqmjmvoci2xb9gzv3fjdf7ns8jgs',
     // Authorization: process.env.CONFIG_AUTH,
-    Authorization: 'Bearer e91sv2wz3vyopq95y6nfkpobyyud2j',
+    Authorization: 'Bearer leyg1kbzubdspq9uiqhcwxbw4j4gq9',
   },
 });
 
